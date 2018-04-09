425f13f88a15fb885d4b44d4952e6d7e581574da
‘Naumov1889’ <maksim.naumov1889@gmail.com>
2018-04-09 18:43:24 +0500

    Ask and footer are done. Now corrections time.

---
+++ src/js/common.js
@@ -0,0 +1,4 @@
+document.querySelector('.ask__form--hide').addEventListener('click', function () {
+	document.querySelector('.ask__form--shown').style.display = 'none';
+	document.querySelector('.ask__form--hidden').style.display = 'flex'
+});
